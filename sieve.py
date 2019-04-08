de5ec148b38781eb02bb940942acb38a519cd8cd
Laura <Lhuiefiction@gmail.com>
2019-04-08 12:54:30 -0400

    First commit~~

---
+++ sieve.py
@@ -0,0 +1,21 @@
+rowA=["A"]*9
+rowB=["B"]*9
+rowC=["C"]*9
+rowD=["D"]*9
+rowE=["E"]*9
+rowF=["F"]*9
+rowG=["G"]*9
+rowH=["H"]*9
+rowI=["I"]*9
+col1=["1"]*9
+col2=["2"]*9
+col3=["3"]*9
+col4=["4"]*9
+col5=["5"]*9
+col6=["6"]*9
+col7=["7"]*9
+col8=["8"]*9
+col9=["9"]*9
+
+#first goal, print squares.
+print (rowA)
